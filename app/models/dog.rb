29899278558eddd0b7a2512eead4dfd3a1b4e57a
Certil Remy <certil@noukod.com>
2019-01-09 15:53:42 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,11 +1,11 @@
 require 'pry'
 class Dog 
- attr_accessor :name,:bread,:age
+ attr_accessor :name,:breed,:age
  @@all =[]
 
- def initialize ( name,bread, age)
+ def initialize ( name,breed, age)
    @name=name
-   @bread=bread
+   @bread=breed
    @age=age  
  end
 end
